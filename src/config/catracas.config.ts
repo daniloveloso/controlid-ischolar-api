03037f8f6d2ad149315c33e1c81b97c06d435d6a
daniloveloso <danilovloso@gmail.com>
2026-07-02 23:25:33 -0300

    Refactor CATRACAS config to use placeholders
    Updated CATRACAS configuration to use placeholders for name and IP.

--- src/config/catracas.config.ts
+++ src/config/catracas.config.ts
@@ -1,22 +1,6 @@
-export const CATRACAS = [
+export const CATRACAS = [ //Lista dos nomes e ips das catracas
   {
-    nome: "CATRACA EFAI 01",
-    ip: "192.168.10.240",
-  },
-  {
-    nome: "CATRACA EFAI 02",
-    ip: "192.168.10.234",
-  },
-  {
-    nome: "CATRACA EM 01",
-    ip: "192.168.10.45",
-  },
-  {
-    nome: "CATRACA EM 02",
-    ip: "192.168.10.142",
-  },
-  {
-    nome: "CATRACA EM 03",
-    ip: "192.168.10.185",
-  }
+    nome: "NOME DA CATRACA",
+    ip: "IP DA CATRACA",
+  } ...
 ];
